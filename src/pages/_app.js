1cb5d9bc3f91ee4c97b3ae95449bde8a3da3130a
abdullahmeer12 <mirabdullah0077@gmail.com>
2024-06-25 00:07:55 +0500

    deploye changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/globals.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  const onRedirectCallback = (appState) => {
+    router.push(appState?.returnTo || '/'); 
+  };
+
+  return (
+    <Auth0Provider
+      domain="dev-pe1d8oowhknqgbpb.us.auth0.com"
+      clientId="TFK1JBFrZokx2WqJjL0yBnhtnzAp3XwF"
+      redirectUri={typeof window !== 'undefined' ? window.location.origin : ''}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
